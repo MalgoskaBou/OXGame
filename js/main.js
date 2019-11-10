cbf5ac6fe7f8682cc9be173f860f9bf47f913305
Daria <daria359@gmail.com>
2019-11-10 02:05:43 +0100

    Change validField and functions

--- js/main.js
+++ js/main.js
@@ -12,9 +12,11 @@ const validNames = function (name1, name2) {
     return false;
 }
 
-// Function checks if board field given as an argument is already taken
-const takenField = function (field) {
-    return field.classList.contains('icon-x') || field.classList.contains('icon-o');
+// Function checks if board field given as an argument is valid. Returns true if field is avaliable.
+const validField = function (field) {
+    if (!field.classList.contains('icon-x') && !field.classList.contains('icon-o')) return true;
+    showAlert(`This field is already taken. Choose another one.`);
+    return false;
 }
 
 // Function shows a message given as an argument
@@ -24,14 +26,14 @@ const showAlert = function (message) {
 
 // If field is available function adds the right figure on a board field depending on the current turn
 const pickField = function (e) {
-    if (!takenField(e.target)) {
+    if (validField(e.target)) {
         e.target.classList.add(Boolean(currentTurn) ? 'icon-o' : 'icon-x');
         changeTurn();
-    } else showAlert('This field is already taken. Choose another one.')
+    }
 }
 
 
-// written just to test if everything is working as expected ;)
+// written to test if everything is working as expected 
 let currentTurn = Math.floor(Math.random() * 2);
 const changeTurn = function () {
     currentTurn = currentTurn ? --currentTurn : ++currentTurn;

9bf284e5f56c44dacdc1d75ba0d43f7ba34674e9
MalgoskaG <ghouga.m@gmail.com>
2019-11-10 21:58:50 +0100

    Merge both js files and commented out doubled functions

--- js/main.js
+++ js/main.js
@@ -63,9 +63,33 @@ const pickField = function (e) {
 
 // written to test if everything is working as expected 
 let currentTurn = Math.floor(Math.random() * 2);
-const changeTurn = function () {
-}
+// const changeTurn = function () {
+// }
 
     currentTurn = currentTurn ? --currentTurn : ++currentTurn;
 // Add listening on every board field
 boardFields.forEach(field => field.addEventListener('click', pickField));
+
+//IWONA
+
+//let currentTurn = 0;
+let name1 = document.getElementById("player1").value;
+let name2 = document.getElementById("player2").value;
+
+//turn change, puts the name of the player to play
+
+//boardFields.addEventListener('click', changeTurn);
+
+function changeTurn (e){
+    if (currentTurn === 0){
+        currentTurn++;
+        putMessage(`It's ${name2}'s turn.`);
+    }else{
+        currentTurn--;
+        putMessage(`It's ${name1}'s turn.`);
+    }
+};
+
+function putMessage(msg){
+    document.getElementById('turn').innerHTML = msg;
+};

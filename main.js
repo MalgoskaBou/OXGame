7b14ddcffc1e0d0b9d0b38acefcad53a175e786b
Crazy Sheep <iv.palka@gmail.com>
2019-11-10 15:48:42 +0100

    delete aditional functions

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-let player1 = 'X';
-let player2 = 'O';
-let currentTurn = 1;
-const square = document.querySelectorAll('.item');
+
+let currentTurn = 0;
 let name1 = document.getElementById("player1").value;
 let name2 = document.getElementById("player2").value;
-// change turn
 
-square.addEventListener('click', changeTurn);
+//turn change, puts the name of the player to play
+
+boardFields.addEventListener('click', changeTurn);
 
 function changeTurn (e){
-    if (currentTurn === 1){
-        event.target.innerHTML = player1;
+    if (currentTurn === 0){
         currentTurn++;
-        putMessage(`It's ${name2} turn.`);
+        putMessage(`It's ${name2}'s turn.`);
     }else{
-        event.target.innerHTML = player2;
         currentTurn--;
-        putMessage(`It's ${name1} turn.`);
+        putMessage(`It's ${name1}'s turn.`);
     }
-}
+};
 
 function putMessage(msg){
     document.getElementById('turn').innerHTML = msg;
-}
+};
